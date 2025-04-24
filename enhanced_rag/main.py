fccc249ec41a4225f5ce1d854c2630dee4cb83a6
Madhav Girish Nair <44905647+MadhavGNair@users.noreply.github.com>
2025-04-24 18:59:26 +0200

    feat: vanilla-RAG implemented with Chroma (temporary)

--- enhanced_rag/main.py
+++ enhanced_rag/main.py
@@ -1,8 +1,28 @@
+import os
+from dotenv import load_dotenv
+
 from self_route import SelfRoute
 from long_context import LongContext
 from vanilla_rag import VanillaRAG
 from enhanced_rag import EnhancedRAG
 
+load_dotenv()
 
-self_route = SelfRoute()
-self_route.load_documents('documents')
+model_choices = ['openai', 'anthropic', 'gemini']
+
+vanilla_rag = VanillaRAG('tests/1682954_thesis_proposal.pdf', 'gpt-4o-mini', os.getenv('OPENAI_API_KEY'), model_choices[0])
+
+results = vanilla_rag.query('What is name of the author of the thesis?')
+
+
+print(results["context"][0].page_content)
+
+print('-' * 100)
+print('\n\n')
+
+print(results["context"][0].metadata)
+
+print('-' * 100)
+print('\n\n')
+
+print(results['answer'])
--- enhanced_rag/vanilla_rag/vanilla_rag.py
+++ enhanced_rag/vanilla_rag/vanilla_rag.py
@@ -1,6 +1,63 @@
-class VanillaRAG:
-    def __init__(self):
-        pass
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_google_vertexai import ChatVertexAI
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
 
-    def load_documents(self, documents):
-        pass
+
+class VanillaRAG:
+    def __init__(self, pdf_path: str, model_name: str, api_key: str, parent_model: str = 'openai'):
+        # load and split the pdf
+        self.pdf_path = pdf_path
+        self.loader = PyPDFLoader(pdf_path)
+        self.docs = self.loader.load()
+
+        # initialize the model
+        if parent_model == 'openai':
+            self.model_name = model_name
+            self.api_key = api_key
+            self.llm = ChatOpenAI(model_name=self.model_name, api_key=self.api_key)
+        elif parent_model == 'anthropic':
+            self.model_name = model_name
+            self.api_key = api_key
+            self.llm = ChatAnthropic(model_name=self.model_name, api_key=self.api_key)
+        elif parent_model == 'gemini':
+            self.model_name = model_name
+            self.api_key = api_key
+            self.llm = ChatVertexAI(model_name=self.model_name, api_key=self.api_key)
+        else:
+            raise ValueError(f"Invalid parent model: {parent_model}")
+
+        # initialize the system prompt
+        self.system_prompt = """
+        You are a helpful assistant that can answer questions about the document. Use the provided context to answer the question. If you don't know the answer, say "I don't know". Be concise and to the point.\n\n{context}
+        """
+
+    def __generate_embeddings(self):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        chunks = text_splitter.split_documents(self.docs)
+        vector_store = Chroma.from_documents(
+            documents=chunks,
+            embedding=OpenAIEmbeddings(model="text-embedding-3-small"),
+        )
+        return vector_store.as_retriever()
+
+    def query(self, query: str):
+        retriever = self.__generate_embeddings()
+        prompt = ChatPromptTemplate.from_messages([
+            ("system", self.system_prompt),
+            ("user", "{input}"),
+        ])
+        q_and_a_chain = create_stuff_documents_chain(
+            llm=self.llm,
+            prompt=prompt,
+        )
+        chain = create_retrieval_chain(
+            retriever, q_and_a_chain
+        )
+        return chain.invoke({"input": query})
